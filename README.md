dabbfdc030d0e152c10ca0580f7d9ba11cd7523d
yihong0618 <zouzou0208@gmail.com>
2021-01-11 22:15:08 +0800

    add: read comment

--- README.md
+++ README.md
@@ -81,6 +81,10 @@
 - [《送你一朵小红花》](https://github.com/yihong0618/2021/issues/2#issuecomment-757455042) 2021-01-10
 <!--END_SECTION:my_movie-->
 
+## 读书
+
+<!--START_SECTION:my_read-->
+<!--END_SECTION:my_read-->
 
 ## 做饭
 
--- main.py
+++ main.py
@@ -12,14 +12,19 @@ COOK_LABEL_LIST = [
 MOVIE_LABEL_LIST = [
     "Movie",
 ]
+READ_LABEL_LIST = [
+    "Read",
+]
 MY_BLOG_REPO = "yihong0618/gitblog"
 GITHUB_README_COMMENTS = (
     "(<!--START_SECTION:{name}-->\n)(.*)(<!--END_SECTION:{name}-->\n)"
 )
 
+# add new label here
 LABEL_DICT = {
     "Cook": {"label_list": COOK_LABEL_LIST, "comment_name": "my_cook"},
-    "Movie": {"label_list": MOVIE_LABEL_LIST, "comment_name": "my_movie"}
+    "Movie": {"label_list": MOVIE_LABEL_LIST, "comment_name": "my_movie"},
+    "Read": {"label_list": READ_LABEL_LIST, "comment_name": "my_read"}
 }
 
 
