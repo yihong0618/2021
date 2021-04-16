1803c0bf68d52d8f0f191e494584372149899bd7
yihong0618 <zouzou0208@gmail.com>
2021-04-17 04:02:25 +0800

    fix: early time

--- get_up.py
+++ get_up.py
@@ -39,7 +39,8 @@ def get_today_get_up_status(issue):
 def make_get_up_message():
     sentence = get_one_sentence()
     now = pendulum.now(TIMEZONE)
-    is_get_up_early = 4 <= now.hour <= 6
+    # 3 - 6 means early for me
+    is_get_up_early = 3 <= now.hour <= 6
     get_up_time = now.to_datetime_string()
     body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
     return body, is_get_up_early
