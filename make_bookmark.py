163cc01ff531510f8f8bc3de67adf4190c82eb7c
yihong0618 <zouzou0208@gmail.com>
2021-09-26 17:26:03 +0800

    fix: link

--- make_bookmark.py
+++ make_bookmark.py
@@ -44,7 +44,7 @@ def main(github_token, repo_name):
         name, link = comment_str_list[0], comment_str_list[1]
         if link.find(f"{repo_name}/{YEAR}/files") != -1:
             has_file = True
-        bookmark_str += make_bookmark_str(name, link, str(c.created_at)[:10], str(c.updated_at)[:10], has_file)
+        bookmark_str += make_bookmark_str(f"[name](link)", c.html_url, str(c.created_at)[:10], str(c.updated_at)[:10], has_file)
     with open(BOOKMARK_FILE_NAME, "w+") as f:
         f.write(BOOKMARK_FILE_HEAD)
         f.write(bookmark_str)
